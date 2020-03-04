6726d62ddd8e936510c38fa41128c7ae71ed0cda
Dmytro <dimonluk2.0@gmail.com>
2020-03-04 22:44:12 +0200

    Fix dockerfile Fix settings

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -15,7 +15,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.environ.get("POSTGRES_NAME"),
-        "USER": os.environ.get("POSTGRES_PASSWORD"),
+        "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
         "PORT": os.environ.get("POSTGRES_PORT"),
