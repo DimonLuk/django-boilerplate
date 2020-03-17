6131ef08d57716f650c373c1898e1c5f46318f53
Dmytro <dimonluk2.0@gmail.com>
2020-03-17 23:50:11 +0200

    Add possibility to rebuild docker container

--- helpers/management/commands/run_in_docker.py
+++ helpers/management/commands/run_in_docker.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
+
 from ..docker_compose import (
-    RunAppTerminalCommand,
+    RemoveDockerImage,
     RunAppInShellTerminalCommand,
+    RunAppTerminalCommand,
 )
 
 
@@ -13,15 +15,20 @@ class Command(BaseCommand):
         " dropped into container shell where all the requirements are installed"
         " and environmental variables are set. By providing this flag application will"
         " be launched in background mode. Hot reloading is configured"
+        "--rebuild - by default already built image is used. This flag starts"
+        "building of new image without any cache"
     )
 
     def add_arguments(self, parser):
         parser.add_argument(
             "--noninteractive", dest="noninteractive", action="store_true"
         )
+        parser.add_argument("--rebuild", dest="rebuild", action="store_true")
         parser.set_defaults(noninteractive=False)
 
     def handle(self, *args, **options):
+        if options["rebuild"]:
+            RemoveDockerImage().start()
         if options["noninteractive"]:
             RunAppTerminalCommand().start()
         else:
